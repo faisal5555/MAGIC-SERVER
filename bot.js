5f38d97f58b26055e1973ee46c5587b67c94de48
faisal5555 <44726870+faisal5555@users.noreply.github.com>
2018-11-14 23:02:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -484,5 +484,18 @@ client.on('message', async message =>{
 
 
 
+client.on('message', function(message) {
+    if (message.content == "مسح") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
+        }
+    }
+
+});
+	
+
 
 client.login(process.env.BOT_TOKEN);
